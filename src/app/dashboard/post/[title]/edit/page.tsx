ef455c29ee96a9aeca8313ce42e78af762b0441b
RikuNomura0710 <riku_fly_fr1007@yahoo.co.jp>
2023-11-02 20:53:02 +0900

    fix

--- src/app/dashboard/post/[title]/edit/page.tsx
+++ src/app/dashboard/post/[title]/edit/page.tsx
@@ -1,5 +1,4 @@
 import { paramCase } from 'src/utils/change-case';
-import axios, { endpoints } from 'src/utils/axios';
 
 import { PostEditView } from 'src/sections/blog/view';
 
@@ -27,10 +26,18 @@ export default function PostEditPage() {
   return <PostEditView title={title} />;
 }
 
-export async function generateStaticParams() {
-  const res = await axios.get(endpoints.post.list);
+// export async function generateStaticParams() {
+//   const res = await axios.get(endpoints.post.list);
 
-  return res.data.posts.map((post: { title: string }) => ({
+//   return res.data.posts.map((post: { title: string }) => ({
+//     title: paramCase(post.title),
+//   }));
+// }
+export async function generateStaticParams() {
+  // ダミーデータを作成します
+  const dummyPosts = [{ title: 'dummy-title-1' }, { title: 'dummy-title-2' }];
+
+  return dummyPosts.map((post: { title: string }) => ({
     title: paramCase(post.title),
   }));
 }
